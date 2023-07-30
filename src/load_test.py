014c199ff1a7833ec520e206de323b151a3fb60e
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-07-30 20:04:42 +0700

    Update load_test.py

--- src/load_test.py
+++ src/load_test.py
@@ -21,7 +21,7 @@ def send_data_to_server(data, url):
 
 #take list file 
 
-url = 'http://localhost:8000/phase-2/prob-2/predict'
+url = 'http://localhost:5040/phase-2/prob-2/predict'
 root_path = "/mnt/e/mlops-marathon/new_mlops/data_warehouse/captured_data/phase-2/prob-2/0/*.parquet"
 
 len = 0
@@ -50,8 +50,8 @@ for file_path in tqdm(glob.glob(root_path), ncols=100, desc ="Loading...", unit 
         if response.json()["drift"] == 1:
              drift +=1
 
-        # if id == 10:
-        #      break
+        if id == 10:
+             break
 print("num drift: ", drift)
         
 
