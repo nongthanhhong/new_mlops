facde86f2202dc3e08451635fe357176634c2dc1
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-08-23 23:25:09 +0700

    Update load_test.py

--- src/load_test.py
+++ src/load_test.py
@@ -129,21 +129,28 @@ def test_load(url, root_path, num_requests):
     avg.append(accuracy)
   print(f"AVG: {np.mean(avg)}")
 
-#take list file 
-# url = 'http://localhost:8000/phase-3/prob-1/predict'
-url = 'http://localhost:5040/phase-3/prob-1/predict'
-root_path = "load_test/phase-3/prob-1/*.parquet"
-# root_path = "data_warehouse/captured_data/phase-2//prob-1/13/*.parquet"
-num_requests = 90
-test_load(url, root_path, num_requests)
 
-# url = 'http://localhost:8000/phase-3/prob-2/predict'
-url = 'http://localhost:5040/phase-3/prob-2/predict'
-root_path = "load_test/phase-3/prob-2/*.parquet"
-# root_path = "data_warehouse/captured_data/phase-2/prob-2/13/*.parquet"
-num_requests = 90
-test_load(url, root_path, num_requests)
-      
+import argparse
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--port", type=int, default="8000")
+  args = parser.parse_args()
+  #take list file 
+  # url = 'http://localhost:8000/phase-3/prob-1/predict'
+  
+  url = f'http://localhost:{args.port}/phase-3/prob-1/predict'
+  root_path = "load_test/phase-3/prob-1/*.parquet"
+  # root_path = "data_warehouse/captured_data/phase-2//prob-1/13/*.parquet"
+  num_requests = 90
+  test_load(url, root_path, num_requests)
+
+  # url = 'http://localhost:8000/phase-3/prob-2/predict'
+  url = f'http://localhost:{args.port}/phase-3/prob-2/predict'
+  root_path = "load_test/phase-3/prob-2/*.parquet"
+  # root_path = "data_warehouse/captured_data/phase-2/prob-2/13/*.parquet"
+  num_requests = 90
+  test_load(url, root_path, num_requests)
+        
 
 
         
